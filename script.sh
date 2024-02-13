f6a8c89aabee4f25336b469d4c47705b85ef6c65
makiritare <blacktower.ashaman@gmail.com>
2024-02-12 21:52:05 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-# Attempt to read the current setting
+# Attempt to read the current setting, defaulting to false if not set
 currentSetting=$(defaults read com.apple.WindowManager StandardHideDesktopIcons)
 
 if [ "$currentSetting" = "1" ]; then
@@ -8,3 +8,5 @@ else
     # If currently false (0) or not set, set to true
     defaults write com.apple.WindowManager StandardHideDesktopIcons -bool true
 fi
+
+# you may need to restart a process or log out/in for changes to take effect.
